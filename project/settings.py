57eba800b08dbcea46e19ae996fd6d9ea917c54c
hom prasad dhakal <homprasaddhakal@gmail.com>
2024-03-01 19:55:34 +0545

    APP accounts

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bhk7bi^-t-qx6@jjde38$6_nu+f!$vy$)!b^pwdg8n#$pkr9-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
